57f16cc734600a0288caf515e86a33e1aff9f559
Andreas Severin Nielsen <zbcannie43@zbc.dk>
2020-02-18 13:01:13 +0100

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODP_Composite
+{
+    public class Client
+    {
+
+        int id;
+
+        public void DoTask()
+        {
+            id = 0;
+            Component composite2 = new Component(GetNewId());
+            composite2.Add(new Component(GetNewId()));
+            composite2.Add(new Component(GetNewId()));
+            composite2.Add(new Component(GetNewId()));
+            Component composite1 = new Component(GetNewId());
+            composite1.Add(new Component(GetNewId()));
+            composite1.Add(composite2);
+            composite1.Add(new Component(GetNewId()));
+
+            Console.WriteLine("Composite 1: " + composite1.Concat());
+            Console.WriteLine("Composite 2: " + composite2.Concat());
+            Console.ReadKey();
+        }
+
+        private int GetNewId()
+        {
+            id++;
+            return id;
+        }
+    }
+}
---
+++ Component.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODP_Composite
+{
+    public class Component : IEnumerable<Component>
+    {
+        public int ID { get; set; }
+
+        private List<Component> children;
+
+        public Component(int iD)
+        {
+            ID = iD;
+            children = new List<Component>();
+        }
+
+        public string Concat()
+        {
+            string txt = GetName() + " ";
+            foreach (Component child in children)
+            {
+                txt += child.Concat() + " ";
+            }
+            return txt;
+        }
+
+        private string GetName()
+        {
+            string txt;
+            if (children.Count == 0) txt = "leaf";
+            else txt = "composite";
+            return txt + ID.ToString();
+        }
+
+        public void Add(Component child)
+        {
+            children.Add(child);
+        }
+
+        public IEnumerator<Component> GetEnumerator()
+        {
+            return ((IEnumerable<Component>)children).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Component>)children).GetEnumerator();
+        }
+    }
+}
